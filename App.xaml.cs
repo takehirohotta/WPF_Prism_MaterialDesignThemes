7da7bebb5fa6fcb84549acbfbbed39d075d9e153
MA <kflwwa@gmail.com>
2025-06-08 20:24:11 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Unity;
+using WPFprism.Views;
+
+namespace WPFprism
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+
+namespace WPFprism.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
